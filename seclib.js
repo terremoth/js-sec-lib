cf31c9d8839bc47d9e9dde58f5c0d0cb8ebbd5ae
terremoth <dutr4@outlook.com>
2017-09-09 04:19:25 +0100

    md5, sha1, sha256 functions

---
+++ seclib.js
@@ -0,0 +1,414 @@
+var md5 = function (s) {
+    function L(k, d) {
+        return(k << d) | (k >>> (32 - d));
+    }
+    function K(G, k) {
+        var I, d, F, H, x;
+        F = (G & 2147483648);
+        H = (k & 2147483648);
+        I = (G & 1073741824);
+        d = (k & 1073741824);
+        x = (G & 1073741823) + (k & 1073741823);
+        if (I & d) {
+            return(x ^ 2147483648 ^ F ^ H);
+        }
+        if (I | d) {
+            if (x & 1073741824) {
+                return(x ^ 3221225472 ^ F ^ H);
+            } else {
+                return(x ^ 1073741824 ^ F ^ H);
+            }
+        } else {
+            return(x ^ F ^ H);
+        }
+    }
+    function r(d, F, k) {
+        return(d & F) | ((~d) & k);
+    }
+    function q(d, F, k) {
+        return(d & k) | (F & (~k));
+    }
+    function p(d, F, k) {
+        return(d ^ F ^ k);
+    }
+    function n(d, F, k) {
+        return(F ^ (d | (~k)));
+    }
+    function u(G, F, aa, Z, k, H, I) {
+        G = K(G, K(K(r(F, aa, Z), k), I));
+        return K(L(G, H), F);
+    }
+    function f(G, F, aa, Z, k, H, I) {
+        G = K(G, K(K(q(F, aa, Z), k), I));
+        return K(L(G, H), F);
+    }
+    function D(G, F, aa, Z, k, H, I) {
+        G = K(G, K(K(p(F, aa, Z), k), I));
+        return K(L(G, H), F);
+    }
+    function t(G, F, aa, Z, k, H, I) {
+        G = K(G, K(K(n(F, aa, Z), k), I));
+        return K(L(G, H), F);
+    }
+    function e(G) {
+        var Z;
+        var F = G.length;
+        var x = F + 8;
+        var k = (x - (x % 64)) / 64;
+        var I = (k + 1) * 16;
+        var aa = Array(I - 1);
+        var d = 0;
+        var H = 0;
+        while (H < F) {
+            Z = (H - (H % 4)) / 4;
+            d = (H % 4) * 8;
+            aa[Z] = (aa[Z] | (G.charCodeAt(H) << d));
+            H++;
+        }
+        Z = (H - (H % 4)) / 4;
+        d = (H % 4) * 8;
+        aa[Z] = aa[Z] | (128 << d);
+        aa[I - 2] = F << 3;
+        aa[I - 1] = F >>> 29;
+        return aa;
+    }
+    function B(x) {
+        var k = "", F = "", G, d;
+        for (d = 0; d <= 3; d++) {
+            G = (x >>> (d * 8)) & 255;
+            F = "0" + G.toString(16);
+            k = k + F.substr(F.length - 2, 2);
+        }
+        return k;
+    }
+    function J(k) {
+        k = k.replace(/rn/g, "n");
+        var d = "";
+        for (var F = 0; F < k.length; F++) {
+            var x = k.charCodeAt(F);
+            if (x < 128) {
+                d += String.fromCharCode(x);
+            } else {
+                if ((x > 127) && (x < 2048)) {
+                    d += String.fromCharCode((x >> 6) | 192);
+                    d += String.fromCharCode((x & 63) | 128);
+                } else {
+                    d += String.fromCharCode((x >> 12) | 224);
+                    d += String.fromCharCode(((x >> 6) & 63) | 128);
+                    d += String.fromCharCode((x & 63) | 128);
+                }
+            }
+        }
+        return d;
+    }
+    var C = Array();
+    var P, h, E, v, g, Y, X, W, V;
+    var S = 7, Q = 12, N = 17, M = 22;
+    var A = 5, z = 9, y = 14, w = 20;
+    var o = 4, m = 11, l = 16, j = 23;
+    var U = 6, T = 10, R = 15, O = 21;
+    s = J(s);
+    C = e(s);
+    Y = 1732584193;
+    X = 4023233417;
+    W = 2562383102;
+    V = 271733878;
+    for (P = 0; P < C.length; P += 16) {
+        h = Y;
+        E = X;
+        v = W;
+        g = V;
+        Y = u(Y, X, W, V, C[P + 0], S, 3614090360);
+        V = u(V, Y, X, W, C[P + 1], Q, 3905402710);
+        W = u(W, V, Y, X, C[P + 2], N, 606105819);
+        X = u(X, W, V, Y, C[P + 3], M, 3250441966);
+        Y = u(Y, X, W, V, C[P + 4], S, 4118548399);
+        V = u(V, Y, X, W, C[P + 5], Q, 1200080426);
+        W = u(W, V, Y, X, C[P + 6], N, 2821735955);
+        X = u(X, W, V, Y, C[P + 7], M, 4249261313);
+        Y = u(Y, X, W, V, C[P + 8], S, 1770035416);
+        V = u(V, Y, X, W, C[P + 9], Q, 2336552879);
+        W = u(W, V, Y, X, C[P + 10], N, 4294925233);
+        X = u(X, W, V, Y, C[P + 11], M, 2304563134);
+        Y = u(Y, X, W, V, C[P + 12], S, 1804603682);
+        V = u(V, Y, X, W, C[P + 13], Q, 4254626195);
+        W = u(W, V, Y, X, C[P + 14], N, 2792965006);
+        X = u(X, W, V, Y, C[P + 15], M, 1236535329);
+        Y = f(Y, X, W, V, C[P + 1], A, 4129170786);
+        V = f(V, Y, X, W, C[P + 6], z, 3225465664);
+        W = f(W, V, Y, X, C[P + 11], y, 643717713);
+        X = f(X, W, V, Y, C[P + 0], w, 3921069994);
+        Y = f(Y, X, W, V, C[P + 5], A, 3593408605);
+        V = f(V, Y, X, W, C[P + 10], z, 38016083);
+        W = f(W, V, Y, X, C[P + 15], y, 3634488961);
+        X = f(X, W, V, Y, C[P + 4], w, 3889429448);
+        Y = f(Y, X, W, V, C[P + 9], A, 568446438);
+        V = f(V, Y, X, W, C[P + 14], z, 3275163606);
+        W = f(W, V, Y, X, C[P + 3], y, 4107603335);
+        X = f(X, W, V, Y, C[P + 8], w, 1163531501);
+        Y = f(Y, X, W, V, C[P + 13], A, 2850285829);
+        V = f(V, Y, X, W, C[P + 2], z, 4243563512);
+        W = f(W, V, Y, X, C[P + 7], y, 1735328473);
+        X = f(X, W, V, Y, C[P + 12], w, 2368359562);
+        Y = D(Y, X, W, V, C[P + 5], o, 4294588738);
+        V = D(V, Y, X, W, C[P + 8], m, 2272392833);
+        W = D(W, V, Y, X, C[P + 11], l, 1839030562);
+        X = D(X, W, V, Y, C[P + 14], j, 4259657740);
+        Y = D(Y, X, W, V, C[P + 1], o, 2763975236);
+        V = D(V, Y, X, W, C[P + 4], m, 1272893353);
+        W = D(W, V, Y, X, C[P + 7], l, 4139469664);
+        X = D(X, W, V, Y, C[P + 10], j, 3200236656);
+        Y = D(Y, X, W, V, C[P + 13], o, 681279174);
+        V = D(V, Y, X, W, C[P + 0], m, 3936430074);
+        W = D(W, V, Y, X, C[P + 3], l, 3572445317);
+        X = D(X, W, V, Y, C[P + 6], j, 76029189);
+        Y = D(Y, X, W, V, C[P + 9], o, 3654602809);
+        V = D(V, Y, X, W, C[P + 12], m, 3873151461);
+        W = D(W, V, Y, X, C[P + 15], l, 530742520);
+        X = D(X, W, V, Y, C[P + 2], j, 3299628645);
+        Y = t(Y, X, W, V, C[P + 0], U, 4096336452);
+        V = t(V, Y, X, W, C[P + 7], T, 1126891415);
+        W = t(W, V, Y, X, C[P + 14], R, 2878612391);
+        X = t(X, W, V, Y, C[P + 5], O, 4237533241);
+        Y = t(Y, X, W, V, C[P + 12], U, 1700485571);
+        V = t(V, Y, X, W, C[P + 3], T, 2399980690);
+        W = t(W, V, Y, X, C[P + 10], R, 4293915773);
+        X = t(X, W, V, Y, C[P + 1], O, 2240044497);
+        Y = t(Y, X, W, V, C[P + 8], U, 1873313359);
+        V = t(V, Y, X, W, C[P + 15], T, 4264355552);
+        W = t(W, V, Y, X, C[P + 6], R, 2734768916);
+        X = t(X, W, V, Y, C[P + 13], O, 1309151649);
+        Y = t(Y, X, W, V, C[P + 4], U, 4149444226);
+        V = t(V, Y, X, W, C[P + 11], T, 3174756917);
+        W = t(W, V, Y, X, C[P + 2], R, 718787259);
+        X = t(X, W, V, Y, C[P + 9], O, 3951481745);
+        Y = K(Y, h);
+        X = K(X, E);
+        W = K(W, v);
+        V = K(V, g);
+    }
+    var i = B(Y) + B(X) + B(W) + B(V);
+    return i.toLowerCase();
+};
+
+function sha1(str) {
+    var hash;
+    try {
+        var crypto = require('crypto');
+        var sha1sum = crypto.createHash('sha1');
+        sha1sum.update(str);
+        hash = sha1sum.digest('hex');
+    } catch (e) {
+        hash = undefined;
+    }
+    if (hash !== undefined) {
+        return hash;
+    }
+    var _rotLeft = function (n, s) {
+        var t4 = (n << s) | (n >>> (32 - s));
+        return t4;
+    };
+    var _cvtHex = function (val) {
+        var str = '';
+        var i;
+        var v;
+        for (i = 7; i >= 0; i--) {
+            v = (val >>> (i * 4)) & 0x0f;
+            str += v.toString(16);
+        }
+        return str;
+    };
+    var blockstart;
+    var i, j;
+    var W = new Array(80);
+    var H0 = 0x67452301;
+    var H1 = 0xEFCDAB89;
+    var H2 = 0x98BADCFE;
+    var H3 = 0x10325476;
+    var H4 = 0xC3D2E1F0;
+    var A, B, C, D, E;
+    var temp;
+    // utf8_encode
+    str = unescape(encodeURIComponent(str));
+    var strLen = str.length;
+    var wordArray = [];
+    for (i = 0; i < strLen - 3; i += 4) {
+        j = str.charCodeAt(i) << 24 |
+                str.charCodeAt(i + 1) << 16 |
+                str.charCodeAt(i + 2) << 8 |
+                str.charCodeAt(i + 3);
+        wordArray.push(j);
+    }
+    switch (strLen % 4) {
+        case 0:
+            i = 0x080000000;
+            break
+        case 1:
+            i = str.charCodeAt(strLen - 1) << 24 | 0x0800000;
+            break
+        case 2:
+            i = str.charCodeAt(strLen - 2) << 24 | str.charCodeAt(strLen - 1) << 16 | 0x08000;
+            break
+        case 3:
+            i = str.charCodeAt(strLen - 3) << 24 |
+                    str.charCodeAt(strLen - 2) << 16 |
+                    str.charCodeAt(strLen - 1) <<
+                    8 | 0x80;
+            break
+    }
+    wordArray.push(i);
+    while ((wordArray.length % 16) !== 14) {
+        wordArray.push(0);
+    }
+    wordArray.push(strLen >>> 29);
+    wordArray.push((strLen << 3) & 0x0ffffffff);
+    for (blockstart = 0; blockstart < wordArray.length; blockstart += 16) {
+        for (i = 0; i < 16; i++) {
+            W[i] = wordArray[blockstart + i];
+        }
+        for (i = 16; i <= 79; i++) {
+            W[i] = _rotLeft(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+        }
+        A = H0;
+        B = H1;
+        C = H2;
+        D = H3;
+        E = H4;
+        for (i = 0; i <= 19; i++) {
+            temp = (_rotLeft(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = _rotLeft(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 20; i <= 39; i++) {
+            temp = (_rotLeft(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = _rotLeft(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 40; i <= 59; i++) {
+            temp = (_rotLeft(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = _rotLeft(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 60; i <= 79; i++) {
+            temp = (_rotLeft(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = _rotLeft(B, 30);
+            B = A;
+            A = temp;
+        }
+        H0 = (H0 + A) & 0x0ffffffff;
+        H1 = (H1 + B) & 0x0ffffffff;
+        H2 = (H2 + C) & 0x0ffffffff;
+        H3 = (H3 + D) & 0x0ffffffff;
+        H4 = (H4 + E) & 0x0ffffffff;
+    }
+    temp = _cvtHex(H0) + _cvtHex(H1) + _cvtHex(H2) + _cvtHex(H3) + _cvtHex(H4);
+    return temp.toLowerCase();
+}
+
+var sha256 = function sha256(ascii) {
+	function rightRotate(value, amount) {
+		return (value>>>amount) | (value<<(32 - amount));
+	};
+	
+	var mathPow = Math.pow;
+	var maxWord = mathPow(2, 32);
+	var lengthProperty = 'length';
+	var i, j; // Used as a counter across the whole file
+	var result = '';
+
+	var words = [];
+	var asciiBitLength = ascii[lengthProperty]*8;
+	
+	//* caching results is optional - remove/add slash from front of this line to toggle
+	// Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+	// (we actually calculate the first 64, but extra values are just ignored)
+	var hash = sha256.h = sha256.h || [];
+	// Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+	var k = sha256.k = sha256.k || [];
+	var primeCounter = k[lengthProperty];
+	/*/
+	var hash = [], k = [];
+	var primeCounter = 0;
+	//*/
+
+	var isComposite = {};
+	for (var candidate = 2; primeCounter < 64; candidate++) {
+		if (!isComposite[candidate]) {
+			for (i = 0; i < 313; i += candidate) {
+				isComposite[i] = candidate;
+			}
+			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+		}
+	}
+	
+	ascii += '\x80'; // Append Ƈ' bit (plus zero padding)
+	while (ascii[lengthProperty]%64 - 56) ascii += '\x00'; // More zero padding
+	for (i = 0; i < ascii[lengthProperty]; i++) {
+		j = ascii.charCodeAt(i);
+		if (j>>8) return; // ASCII check: only accept characters in range 0-255
+		words[i>>2] |= j << ((3 - i)%4)*8;
+	}
+	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+	words[words[lengthProperty]] = (asciiBitLength);
+	
+	// process each chunk
+	for (j = 0; j < words[lengthProperty];) {
+		var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+		var oldHash = hash;
+		// This is now the undefinedworking hash", often labelled as variables a...g
+		// (we have to truncate as well, otherwise extra entries at the end accumulate
+		hash = hash.slice(0, 8);
+		
+		for (i = 0; i < 64; i++) {
+			var i2 = i + j;
+			// Expand the message into 64 words
+			// Used below if 
+			var w15 = w[i - 15], w2 = w[i - 2];
+
+			// Iterate
+			var a = hash[0], e = hash[4];
+			var temp1 = hash[7]
+				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
+				+ ((e&hash[5])^((~e)&hash[6])) // ch
+				+ k[i]
+				// Expand the message schedule if needed
+				+ (w[i] = (i < 16) ? w[i] : (
+						w[i - 16]
+						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+						+ w[i - 7]
+						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+					)|0
+				);
+			// This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+			var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+			
+			hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+			hash[4] = (hash[4] + temp1)|0;
+		}
+		
+		for (i = 0; i < 8; i++) {
+			hash[i] = (hash[i] + oldHash[i])|0;
+		}
+	}
+	
+	for (i = 0; i < 8; i++) {
+		for (j = 3; j + 1; j--) {
+			var b = (hash[i]>>(j*8))&255;
+			result += ((b < 16) ? 0 : '') + b.toString(16);
+		}
+	}
+	return result;
+};
